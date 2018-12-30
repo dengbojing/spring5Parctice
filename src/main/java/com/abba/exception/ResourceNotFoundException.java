db6f29bdec2238088404c6c2657bf6bb0f5beeaa
dengbojing <dengbojing@qq.com>
2018-12-30 14:33:33 +0800

    add exception info

--- src/main/java/com/abba/exception/ResourceNotFoundException.java
+++ src/main/java/com/abba/exception/ResourceNotFoundException.java
@@ -1,5 +1,11 @@
 package com.abba.exception;
 
+/**
+ * @author dengbojing
+ */
 public class ResourceNotFoundException extends Throwable{
-
+    @Override
+    public String getMessage(){
+        return "";
+    }
 }
